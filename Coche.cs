145228a3f12b8fc8fe78811a975aeee7623d9ce0
XZifer <lic.luisfsanchezbarrera@gmail.com>
2022-06-26 11:39:33 -0500

    Clase Vehiculo y Coche con su cambio de precio

---
+++ Coche.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POOHerencia
+{
+    class Coche:Vehiculo
+    {
+        private bool _airbag { get; set; }
+        public Coche(int id,string marca,string modelo,int km,double precio,bool airbag)
+            :base(id,marca,modelo,km,precio) 
+        {
+            this._airbag = airbag;
+        }
+        public bool airbag { get => _airbag; set => _airbag = value; }
+
+        public override double precio 
+        {
+            get
+            {
+                if (this.airbag)
+                {
+                    return base.precio + 200;
+                }
+                else
+                {
+                    return base.precio;
+                }
+            } 
+        }
+
+    }
+}
---
+++ Vehiculo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POOHerencia
+{
+    class Vehiculo
+    {
+        private int _id;
+        private string _marca;
+        private string _modelo;
+        private int _km;
+        private double _precio;
+
+        public Vehiculo(int id,string marca,string modelo,int km,double precio)
+        {
+            this._id = id;
+            this._marca = marca;
+            this._modelo = modelo;
+            this._km = km;  
+            this._precio = precio;
+        }
+        public int id { get => _id; set => _id = value; }
+        public string marca { get => _marca; set => _marca = value; }
+        public string modelo { get => _modelo; set => _modelo = value; }
+        public int km { get => _km; set => _km = value; }
+        public virtual double precio { get => _precio; set => _precio = value; }
+
+        public override string ToString()
+        {
+            return "Detalles\n" + marca + "\n" + modelo + "\n" + km + "kms\n" + "$" + precio;
+        }
+    }
+}
