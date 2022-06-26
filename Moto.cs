f119d4407003d5d3eba8f6d59c06ffd1bd4944b3
XZifer <lic.luisfsanchezbarrera@gmail.com>
2022-06-26 11:47:26 -0500

    Clase Moto con su precio modificado

---
+++ Moto.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POOHerencia
+{
+    class Moto:Vehiculo
+    {
+        private bool _sideCard { get; set; }
+        public Moto(int id, string marca, string modelo, int km, double precio, bool sideCard)
+            : base(id, marca, modelo, km, precio)
+        {
+            this._sideCard = sideCard;
+        }
+        public bool sideCard { get => _sideCard; set => _sideCard = value; }
+
+        public override double precio
+        {
+            get
+            {
+                if (this.sideCard)
+                {
+                    return base.precio + 50;
+                }
+                else
+                {
+                    return base.precio;
+                }
+            }
+        }
+    }
+}
